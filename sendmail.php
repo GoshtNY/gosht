604b804cef4629fc528175b90758c0ab2e531d2e
GoshtNY <98841658+GoshtNY@users.noreply.github.com>
2022-02-21 00:14:46 +0500

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,7 @@
 <?php
-
+echo "11111111111";
 if(isset($_POST['submit'])){
+    echo "2222222222222222222";
     $to = "info@gosht.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
@@ -12,7 +13,9 @@ if(isset($_POST['submit'])){
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
+    echo "3333333";
     mail($to,$subject,$message,$headers);
+    echo "444444";
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     header("Location: http://www.gosht.com/");
