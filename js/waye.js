cb522cef02daa9ed902fdee0c371649b8b06349d
GoshtNY <98841658+GoshtNY@users.noreply.github.com>
2022-03-01 16:05:05 +0500

    Update waye.js

--- js/waye.js
+++ js/waye.js
@@ -189,32 +189,40 @@ $(function() {
                         "inputError"), s = !0);
                 }
             }), !s) {
-            $("form#form input.submit").fadeOut("normal", function() {
-		    alert("11111");
-                $(this).parent().append("");
-		    alert("22222");
-            });
+		
+//             $("form#form input.submit").fadeOut("normal", function() {
+// 		    alert("11111");
+//                 $(this).parent().append("");
+// 		    alert("22222");
+//             });
+		
 		alert("3333");
             var r = $(this).serialize();
 		alert("4444");
-		
+
+			
+		const form = document.getElementById('form');
+		form.method = 'POST';
+		form.action = 'https://formspree.io/f/mnqwvpdo';
+		form.submit();
+
 
 		
-	    $.post($(this).attr("action"), r, function() {
-		    alert("88888");
-                $("form#form").slideUp("fast", function() {
-			alert("666666");
-                    $(this).before('<div class="success">Your email was sent successfully.</div>');
-			alert("77777");
-                });
-            }).done(function() {
-    		alert( "second success" );
-  	    }).fail(function() {
-    		alert( "error" );
-  	    }).always(function() {
-   		alert( "finished" );
-            });
-	    alert("55555");
+// 	    $.post($(this).attr("action"), r, function() {
+// 		    alert("88888");
+//                 $("form#form").slideUp("fast", function() {
+// 			alert("666666");
+//                     $(this).before('<div class="success">Your email was sent successfully.</div>');
+// 			alert("77777");
+//                 });
+//             }).done(function() {
+//     		alert( "second success" );
+//   	    }).fail(function() {
+//     		alert( "error" );
+//   	    }).always(function() {
+//    		alert( "finished" );
+//             });
+// 	    alert("55555");
         }
         return !1;
     });
